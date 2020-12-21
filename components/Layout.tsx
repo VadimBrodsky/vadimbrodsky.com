504b844371ad6b41be017206d2ac2e28112a0abc
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-12-20 21:07:05 -0500

    Use new components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,37 +1,27 @@
-import Head from 'next/head';
-
+import Footer from './Footer';
+import Head from './Head';
 import Nav from './Nav';
 
 interface Props {
   children?: React.ReactNode;
   title?: string;
-};
+}
 
 const DEFAULT_TITLE = 'This is the default title';
 
 const Layout: React.FC<Props> = (props): JSX.Element => (
-  <div>
+  <>
     <Head>
       <title>{props.title || DEFAULT_TITLE}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" content="#000000" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6d6d6d" />
     </Head>
-    <header>
-      <Nav/>
-    </header>
-    {props.children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+    <div className="mt-10">
+      <header>
+        <Nav />
+      </header>
+      {props.children}
+      <Footer />
+    </div>
+  </>
 );
 
 export default Layout;
