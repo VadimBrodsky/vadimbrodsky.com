f2aaad9aa24ad228094cd509c3aed012ff02444a
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-12-20 21:06:54 -0500

    Cleanup Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,21 +5,19 @@ import Logo from './Logo';
 const Nav: React.FC = (): JSX.Element => (
   <nav className="container mx-auto text-center">
     <Logo />
-    <Link href="/">
-      <ul className="lowercase text-center">
-        <li className="inline-block">
-          <Link href="/">Blog</Link>
-          <span className="mx-3">/</span>
-        </li>
-        <li className="inline-block">
-          <Link href="/">Projects</Link>
-          <span className="mx-3">/</span>
-        </li>
-        <li className="inline-block">
-          <Link href="/">About</Link>
-        </li>
-      </ul>
-    </Link>
+    <ul className="lowercase text-center mt-10">
+      <li className="inline-block">
+        <Link href="/">Blog</Link>
+        <span className="mx-3">/</span>
+      </li>
+      <li className="inline-block">
+        <Link href="/">Projects</Link>
+        <span className="mx-3">/</span>
+      </li>
+      <li className="inline-block">
+        <Link href="/">About</Link>
+      </li>
+    </ul>
   </nav>
 );
 
